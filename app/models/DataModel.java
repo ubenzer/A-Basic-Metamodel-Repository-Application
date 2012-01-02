30278c6dc2a65826d5440a92130f5f207846548b
Umut Benzer <umutbenzer@gmail.com>
2012-01-02 14:42:16 +0200

    some more models and many to many mappings

--- app/models/DataModel.java
+++ app/models/DataModel.java
@@ -1,12 +1,30 @@
 package models;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import play.db.jpa.Model;
 
 @Entity
 public class DataModel extends Model {
     
-  String name;
+  public String name;
+  
+  @ManyToMany
+  @JoinTable(name="datamodelattribute", 
+      joinColumns={@JoinColumn(name="datamodelFK", referencedColumnName="ID")},
+      inverseJoinColumns={@JoinColumn(name="attributeFK", referencedColumnName="ID")})
+  public Set<Attribute> attributes;
+  
+  @ManyToMany
+  @JoinTable(name="datamodelentity", 
+      joinColumns={@JoinColumn(name="datamodelFK", referencedColumnName="ID")},
+      inverseJoinColumns={@JoinColumn(name="entityFK", referencedColumnName="ID")})
+  public Set<models.Entity> entities;  
+  
 
 }
